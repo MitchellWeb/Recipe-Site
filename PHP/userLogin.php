73e1b76f1939f5e6ced196565ba05abd93cbf89b
Mitchell Nesheim <mitchell.nesheim12@gmail.com>
2022-12-08 05:12:05 -0600

    user login backend created

--- PHP/userLogin.php
+++ PHP/userLogin.php
@@ -1,5 +1,29 @@
 <?php
-    
+include 'connectPDO.php';
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+$sql = "SELECT count(*) FROM user_accounts WHERE user_username = :username AND user_password = :pass"; //check all entries in the user_accounts table for a match of the username and password entered by the user
+
+$stmt = $conn->prepare($sql);//create statment
+
+//bind parameters
+$stmt->bindParam(':username', $username);
+$stmt->bindParam(':pass', $password);
+
+$stmt->execute();
+
+$rowCount = $stmt->fetchColumn();
+
+if($rowCount > 0)
+{
+    echo "Valid User";
+}
+else{
+   echo "Invalid User";
+}
+
 
 
 ?>
