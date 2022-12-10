7294d2ff136342eda91ac7e71cf55659bd53da7d
Mitchell Nesheim <mitchell.nesheim12@gmail.com>
2022-12-10 01:36:09 -0600

    image url column added in database

--- PHP/uploadRecipe.php
+++ PHP/uploadRecipe.php
@@ -6,18 +6,20 @@
     
     $recipeJSON = $_REQUEST['recipe_JSON'];
     $recipeTitle = $_REQUEST['recipe_title'];
+    $recipeImageUrl = $_REQUEST['recipe_image_url'];
 
     
 
     $sql = "INSERT INTO recipe_data";
-    $sql .="(recipe_json, recipe_title)";//
-    $sql .="VALUES (:recipe_json, :recipe_title)";//
+    $sql .="(recipe_json, recipe_title, recipe_image_url)";
+    $sql .="VALUES (:recipe_json, :recipe_title, :recipe_image_url)";
     
 
     $stmt = $conn->prepare($sql);
 
     $stmt->bindParam(':recipe_json', $recipeJSON);
     $stmt->bindParam(':recipe_title', $recipeTitle );
+    $stmt->bindParam(':recipe_image_url', $recipeImageUrl );
 
     $stmt->execute();
     
